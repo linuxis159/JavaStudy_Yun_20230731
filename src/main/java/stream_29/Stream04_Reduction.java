188385f595020d5f3b369dde28ac2f93f97a3d35
Ikgyeong.kim <dlrrud159@gmail.com>
2023-08-02 00:03:55 +0900

    stream

---
+++ src/main/java/stream_29/Stream04_Reduction.java
@@ -0,0 +1,28 @@
+package stream_29;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+public class Stream04_Reduction {
+    // Reduction is operaion reduceing data
+    // using called reduce function
+    public static void main(String[] args){
+        List<String> names = Arrays.asList("Yuu", "Arisa", "Suzuki");
+        // 1.Yuu compare with Arisa
+        // 2.Arisa is more long
+        // 3.Arisa comapre with Suzuki
+        // 4.Suzuki more long
+        // 5.return Suzuki
+        BinaryOperator<String> lc = (s1,s2) -> {
+            if(s1.length() > s2.length())
+                return s1;
+            else
+                return s2;
+        };
+        // reduce fist argument added first array item
+        String str = names.stream().reduce("", lc);
+        System.out.println(str);
+
+    }
+}
